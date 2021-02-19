7942b3be646a06bf8848e49fffbd65d2eaa94e29
David Ariza <david.ariza@pi.edu.co>
2021-02-19 08:47:02 -0500

    se separan funciones del nucleo

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,20 @@
-#se agrega un ciclo para repetir la pregunta
-
+#se separan las funciones del core
+#from calculadoraFuncionesv3 import *
+#import funciones.calculadoraFuncionesv3
+import calculadoraFuncionesv3
 repetir = "si"
 while repetir.lower() == "si":
     n1 = float(input("deme un numero\n"))
     n2 = float(input("deme otro numero\n"))
     operacion = str(input("que operacion desea hacer:\n1.Suma\n2.Resta\n3.Multipliacion\n4.Division\n"))
-    if operacion.lower() == "suma" or int(operacion) == 1:
-        respuesta = n1 + n2
-    elif operacion.lower() == "resta" or int(operacion) == 2:
-        respuesta = n1 - n2
-    elif operacion.lower() == "multiplicacion" or int(operacion) == 3:
-        respuesta = n1 * n2
-    elif operacion.lower() == "division" or int(operacion) == 4:
-        respuesta = n1 / n2
+    if operacion.lower == "suma" or int(operacion) == 1:
+        respuesta = funciones.calculadoraFuncionesv3.sumar(n1,n2)# no va a funcionar sino esta es una carpeta llamada funciones
+    elif operacion.lower == "resta" or int(operacion) == 2:
+        respuesta = calculadoraFuncionesv3.restar(n1, n2)
+    elif operacion.lower == "multiplicacion" or int(operacion) == 3:
+        respuesta = multiplicar(n1,n2)
+    elif operacion.lower == "division" or int(operacion) == 4:
+        respuesta = dividir(n1,n2)
     else:
         print("la opcion elegida no esta en la lista")
     print("la respuesta es {}".format(respuesta))
---
+++ calculadoraFunciones.py
@@ -0,0 +1,15 @@
+def sumar(x,y):
+    respuesta = x + y
+    return respuesta
+
+def restar(x,y):
+    respuesta = x - y
+    return respuesta
+
+def multiplicar(x,y):
+    respuesta = x * y
+    return respuesta
+
+def dividir(x,y):
+    respuesta = x / y
+    return respuesta
